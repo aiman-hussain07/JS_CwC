706a192f49965145a77f20b5d3db7074457bd420
Aiman Hussain <98013447+aiman-hussain07@users.noreply.github.com>
2024-01-23 14:47:32 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const accountId = 1234
+let accountEmail = "xyz@gmail.com"
+var accountPass = "0987"
+accountCity = "New Delhi"
+let accountState;
+
+// accountId = 2345
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
